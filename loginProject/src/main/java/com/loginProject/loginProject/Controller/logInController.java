72208dd3aac8134683f2ec50cffcc63c023981e3
saiapple783 <saisandeepn26@gmail.com>
2024-07-31 11:34:49 -0500

    Added HTTP Response Statuscode

--- loginProject/src/main/java/com/loginProject/loginProject/Controller/logInController.java
+++ loginProject/src/main/java/com/loginProject/loginProject/Controller/logInController.java
@@ -6,6 +6,9 @@ import com.loginProject.loginProject.Model.Login;
 import com.loginProject.loginProject.Service.EmailService;
 import com.loginProject.loginProject.Service.logInService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -21,13 +24,15 @@ public class logInController {
     EmailService Eservice;
 
     @GetMapping("/greet")
-    public String greet(){
-        return "welocme to Login page";
+    public ResponseEntity<String> greet()
+    {
+        return new ResponseEntity<>("welocme to Login page", HttpStatus.OK);
     }
 
 
     //login check
     @PostMapping("/")
+
     public String checkCred(@RequestBody Login loginReq){
         if(service.checkCred(loginReq.getUsername(),loginReq.getPassword())){
             return "You have succesfully logged In";
@@ -39,25 +44,25 @@ public class logInController {
 
     //Registration of user
     @PostMapping("/adduser")
-    public String addUser(@RequestBody Login loginReq){
+    public ResponseEntity<String> addUser(@RequestBody Login loginReq){
         if(service.addUser(loginReq.getUsername(),loginReq.getPassword(),loginReq)){
-            return "You have succesfully created account";
+            return new ResponseEntity<>("You have succesfully created account",HttpStatus.CREATED);
         }
         else{
-            return "User already exist";
+            return new ResponseEntity<>("User already exist",HttpStatus.IM_USED);
         }
     }
 
     //add reset password logic for creating the token
     @PostMapping("/forgotpassword")
-    public String forgotPassword(@RequestBody Map<String, String> payload){
+    public ResponseEntity<String> forgotPassword(@RequestBody Map<String, String> payload){
 
         String email = payload.get("email").trim().toLowerCase();
         Login User= service.findByemail(email);
 
         if(User==null){
             System.out.println("User not found for email: " + email);
-            return "Email not found";
+            return new ResponseEntity<>("Email not found",HttpStatus.NOT_FOUND);
         }
         System.out.println("User found: " + User.getUsername());
         Login token = service.createToken(User);
@@ -69,8 +74,7 @@ public class logInController {
 
 
         Eservice.sendMail(email,resetUrl);
-
-        return "Password reset email sent. Use the following link to reset your password: " + resetUrl;
+        return new ResponseEntity<>("Password reset email sent. Use the following link to reset your password: " + resetUrl,HttpStatus.OK);
     }
 
     //Implemantation of reset password which will bw called after the email is found in the db ot will generate a token
@@ -78,7 +82,7 @@ public class logInController {
 
 
     @PostMapping("/reset-password")
-    public String resetPassword(@RequestBody Map<String, String> payload){
+    public ResponseEntity<String> resetPassword(@RequestBody Map<String, String> payload){
 
         String newPassword= payload.get("password");
         String token = payload.get("token");
@@ -87,21 +91,22 @@ public class logInController {
 
         if(User==null){
             System.out.println("Invalid token");
-            return "Invalid Token";
+            return new ResponseEntity<>("Invalid Token",HttpStatus.NOT_FOUND);
         }
 
         if(User.getExpiryDate().isBefore(LocalDateTime.now())){
-            return "Token has Expired";
+            return new ResponseEntity<>("Token has Expired",HttpStatus.UNPROCESSABLE_ENTITY);
         }
         service.updatePassword(User,newPassword);
 
         Eservice.sendUpdateMail(User.getEmail());
 
-        return "Password has updated successfully";
+        return new ResponseEntity<>("Password has updated successfully",HttpStatus.ACCEPTED);
     }
     //This takes token and new password in requestbody and then updates the password in database and
 
 
+    //next task is to  create a password reset form and take the updated password
 
 
 
